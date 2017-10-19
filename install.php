b822df3d2185b58ac250181afca622028ffa296c
Arch Nous <nous@archlinux.us>
2017-10-20 02:31:58 +0300

    more changes

--- install.php
+++ install.php
@@ -13,18 +13,18 @@ If you want contact with me, you can email me: francisco.dominguez.lerma@gmail.c
 
 Artix can either be installed through the console or the GUI installer. The GUI install is quite straightforward, we'll focus on the console installation procedure here.
 <br><br>
-<h1>Partition (BIOS)</h1>
+<h1>Disk partitioning (BIOS)</h1>
 <br>
-Partition your hard drive (/dev/sda will be used in this guide) with <i>fdisk</i> or <i>cfdisk</i>, the partition numbers and order are at your discretion:
+Partition your hard drive (/dev/sda will be used in this guide) with <b>fdisk</b> or <b>cfdisk</b>, the partition numbers and order are at your discretion:
 <code>cfdisk /dev/sda
 </code>
-Now, format the new partitions, for example as <i>ext4</i>:
-<code>mkfs.ext4 /dev/sda2        <- root partition
-mkfs.ext4 /dev/sda3        <- home partition, optional
-mkfs.ext4 /dev/sda4        <- boot partition, optional
-mkswap /dev/sda1           <- swap partition
+If you want to install side-by-side with other operating systems, you must make some space on the disk by resizing the existing partitions. You may use <b>gparted</b> for this, however detailed instructions are out of the scope of this guide. Next, format the new partitions, for example as <b>ext4</b>:
+<code>mkfs.ext4 -L ROOT /dev/sda2        <- root partition
+mkfs.ext4 -L HOME /dev/sda3        <- home partition, optional
+mkfs.ext4 -L BOOT /dev/sda4        <- boot partition, optional
+mkswap -L SWAP /dev/sda1           <- swap partition
 </code>
-Mount partitions:
+The <b>-L</b> switch assigns labels to the partitions, which helps referring to them later through <b>/dev/disk/by-label</b> without having to remember their numbers. Now, mount your partitions:
 <code>swapon /dev/sda1
 mount /dev/sda2 /mnt
 mount /dev/sda3 /mnt/home  (if created)
@@ -36,11 +36,11 @@ A working internet connection is required and assumed. A wired connection is set
 <code>ping artixlinux.org
 pacman -Syy
 </code>
-Use basestrap to install the <i>base</i> and optionally the <i>base-devel</i> package groups:
+Use basestrap to install the <b>base</b> and optionally the <b>base-devel</b> package groups:
 <code>basestrap /mnt base base-devel
 </code>
-Use genfstab to generate fstab, use <i>-U</i> for UUIDs and <i>-L</i> for disk labels:
-<code>genfstab -U /mnt >>/mnt/etc/fstab
+Use genfstab to generate fstab, use <b>-U</b> for UUIDs and <b>-L</b> for partition labels:
+<code>genfstab -L /mnt >>/mnt/etc/fstab
 </code>
 Check the resulting fstab for errors before rebooting. Now, you can chroot into your new Artix system with:
 <code>artools-chroot /mnt
@@ -48,8 +48,8 @@ Check the resulting fstab for errors before rebooting. Now, you can chroot into 
 <br>
 <h1>Configure the base system</h1>
 <br>
-First, install grub:
-<code>pacman -S grub2
+First, install <b>grub</b> and <b>os-prober</b> (for detecting other installed operating systems):
+<code>pacman -S grub2 os-prober
 grub-install --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 </code>
@@ -64,7 +64,7 @@ Generate locales:
 <code>nano /etc/locale.gen  <- uncomment your locale
 locale-gen
 </code>
-To set the locale systemwide, edit <i>/etc/profile.d/locale.sh</i> (which is sourced by <i>/etc/profile</i>) or <i>/etc/bash/bashrc.d/artix.bashrc</i>; user-specific changes may be made to their respective ~/.bashrc, for example:
+To set the locale systemwide, edit <b>/etc/profile.d/locale.sh</b> (which is sourced by <b>/etc/profile</b>) or <b>/etc/bash/bashrc.d/artix.bashrc</b>; user-specific changes may be made to their respective ~/.bashrc, for example:
 <code>export LANG="en_US.UTF-8"
 export LC_COLLATE="C"
 </code>
@@ -78,27 +78,27 @@ reboot
 </code>
 Once shutdown is complete, remove your installation media. If all went well, you should boot into your new system. Log in as your root to complete the post-installation configuration.
 <br>
-To get a graphical environment you need to install the <i>xorg group</i>:
+To get a graphical environment you need to install the <b>xorg group</b>:
 <code>pacman -S xorg</code>
-Choose your packages, or just install all of them. For the closed-source nvidia drivers you can use the <i>nvidia-lts</i> package, as our default kernel is <i>linux-lts</i>:
+Choose your packages, or just install all of them. For the closed-source nvidia drivers you can use the <b>nvidia-lts</b> package, as our default kernel is <b>linux-lts</b>:
 <br>
 <code>pacman -S nvidia-lts
 </code>
-Older nvidia cards work with the legacy series, <i>nvidia-340xx-lts</i> and <i>nvidia-304xx-lts</i>. If you want to run a custom kernel, you can install the respective nvidia dkms package which ensures all installed kernels get their nvidia modules.<br>
-AMD and Intel cards enjoy excellent (or near-excellent) 3D support with open-source drivers. Read the <a href="https://wiki.archlinux.org/index.php/xorg#Driver_installation" target="_blank">Arch wiki</a>, for information on how Xorg chooses the best available video driver and which one is best suited to your hardware.
+Older nvidia cards work with the legacy series, <b>nvidia-340xx-lts</b> and <b>nvidia-304xx-lts</b>. If you want to run a custom kernel, you can install the respective nvidia dkms package which ensures all installed kernels get their nvidia modules.<br>
+AMD and Intel cards enjoy excellent (or near-excellent) 3D support with open-source drivers. Read the <a href="https://wiki.archlinux.org/index.php/xorg#Driver_installation" target="_blank">Arch wiki</a>, for information on how Xorg chooses the best available video driver and which one is optimal for your hardware.
 <br><br>
 Install networkmanager:
 <code>pacman -S NetworkManager networkmanager-openrc
 rc-update add NetworkManager default
 </code>
-Install a Desktop Environment, for example MATE or LXQt:
-<code>pacman -S mate mate-extra lxqt</code>
-And optionally a session manager, for example LXDM or SDDM:
+Install a desktop environment, for example MATE, LXQt or XFCE4:
+<code>pacman -S mate mate-extra xfce4 xfce4-goodies lxqt</code>
+And optionally a display manager, like LXDM or SDDM:
 <code>pacman -S lxdm displaymanager-openrc
 rc-update add xdm default
 nano /etc/conf.d/xdm   <- edit and set DISPLAYMANAGER="lxdm"
 </code>
-Or you can use .xinitrc to launch your DE manually; edit (or create) "~/.xinitrc" and add <i>exec mate-session</i>. <i>mate-session</i> and some other packages from Arch are compiled against systemd libs even if they don't use them; to satisfy the library link you may install <i>libsystemd-dummy</i> and <i>systemd-dummy</i>
+Or you can use .xinitrc to launch your DE manually; edit (or create) "~/.xinitrc" and add <b>exec mate-session</b>. <b>mate-session</b> and some other packages from Arch are compiled against systemd libs even if they don't use them; to satisfy the library link you may install <b>libsystemd-dummy</b> and <b>systemd-dummy</b>
 
 <?php
 $nexttitle="Download";
